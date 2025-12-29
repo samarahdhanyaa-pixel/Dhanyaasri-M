4a846ec62393140bf8d5f6934d7a520816cc81ba
DHANYAASRI M <ch.ai.u4aid25063@ch.students.amrita.edu>
2025-12-29 21:25:26 +0530

    Create L1_5.py

---
+++ week1/week1/week1/week1/L1_5.py
@@ -0,0 +1,13 @@
+a = int(input("a: "))
+b = int(input("b: "))
+
+print(a + b)
+print(a - b)
+print(a * b)
+
+if a > b:
+    print("a is bigger")
+elif b > a:
+    print("b is bigger")
+else:
+    print("Equal")
