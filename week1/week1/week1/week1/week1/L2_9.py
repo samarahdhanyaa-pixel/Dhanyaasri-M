95cd90e9074bd67baf9445548799b9d62755fa2e
DHANYAASRI M <ch.ai.u4aid25063@ch.students.amrita.edu>
2026-01-05 22:49:46 +0530

    Create L2_9.py

---
+++ week1/week1/week1/week1/week1/L2_9.py
@@ -0,0 +1,7 @@
+score1 = int(input("Enter exam score 1: "))
+score2 = int(input("Enter exam score 2: "))
+
+if (score1 >= 50 and score2 >= 50):
+    print("You passed!")
+else:
+    print("You need to retake some exams")
