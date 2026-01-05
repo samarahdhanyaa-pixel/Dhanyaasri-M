ad48be9d71bc99e278d8ef452e74a386dda3c754
DHANYAASRI M <ch.ai.u4aid25063@ch.students.amrita.edu>
2026-01-05 22:53:03 +0530

    Add temperature conversion and weather advice script

---
+++ week1/week1/week1/week1/week1/L2_10.py
@@ -0,0 +1,14 @@
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print("Temperature in Fahrenheit:", fahrenheit)
+
+if (celsius < 0):
+    print("Very cold! Wear thick jacket")
+elif (celsius <= 15):
+    print("Cold. Wear jacket")
+elif (celsius <= 25):
+    print("Nice weather")
+else:
+    print("Hot! Stay hydrated")
+
